67bed7019fe8bae16163b2c3bf87f46388d1718b
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2025-02-27 13:18:26 -0500

    lemonaide sale

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,6 +22,14 @@
 		Explore a world of creativity, fun, and endless possibilities with us!
 	</p>
 	<p class="mt-4 max-w-2xl text-center text-lg">We sell posters that we make!!</p>
+	<!-- this next one needs to be a nice looking callout showcase -->
+	<div
+		class="gap-2rounded-2xl mt-4 mb-4 flex max-w-2xl flex-col items-center rounded-2xl border-4 border-yellow-200 bg-gradient-to-tl from-green-500 via-purple-800 to-blue-600 p-5 text-center text-lg font-bold"
+	>
+		We are doing a special 1 day only lemonade sale!
+		<div class="text-md w-min rounded-md border-2 border-yellow-200 p-1">February 28th!!</div>
+		$1.00
+	</div>
 	<AudioDisplay />
 	<Card cardData={cards} />
 </div>
